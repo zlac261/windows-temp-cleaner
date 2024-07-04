db83067b56a0c454ae2fca3e3bb3dcda58c3538b
OmarAlaa12330 <omar.alaa12330@gmail.com>
2024-07-04 15:10:11 +0100

    Improve the log display into columns and rows

--- src/log_display.rs
+++ src/log_display.rs
@@ -1,5 +1,5 @@
 use std::path::PathBuf;
-use eframe::egui::{self, Color32};
+use eframe::egui;
 
 pub struct LogDisplay {
     messages: Vec<String>,
@@ -22,21 +22,23 @@ impl LogDisplay {
         self.failed_deletions.push(FailedDeletionFile::new(path, error_message));
     }
 
+
     pub fn show(&self, ui: &mut egui::Ui) {
-        for message in &self.messages {
-            ui.label(egui::RichText::new(message).color(Color32::RED));
-        }
+        egui::Grid::new("failed_deletion_grid")
+            .striped(true)
+            .show(ui, |ui| {
+                ui.label("File Name");
+                ui.label("Error Message");
 
-        if !self.failed_deletions.is_empty() {
-            ui.separator();
-            ui.label(egui::RichText::new("Failed Deletions:").color(Color32::LIGHT_GRAY));
-
-            for failed in &self.failed_deletions {
-                ui.label(egui::RichText::new(format!("{} - Error: {}", failed.path.display(), failed.error_message))
-                    .color(Color32::RED));
-            }
-        }
+                for failed_file in &self.failed_deletions {
+                    ui.end_row();
+                    ui.label(failed_file.path.file_name().unwrap_or_default().to_string_lossy());
+                    ui.label(&failed_file.error_message);
+                }
+            });
     }
+
+
 
     pub fn clear(&mut self) {
         self.messages.clear();
