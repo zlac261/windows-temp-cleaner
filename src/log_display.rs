d3664277888bafa72ca5d07cb83ed7445d7de0e7
OmarAlaa12330 <omar.alaa12330@gmail.com>
2024-07-04 14:24:48 +0100

    Create struct to display logs

--- src/log_display.rs
+++ src/log_display.rs
@@ -1,4 +1,48 @@
 use std::path::PathBuf;
+use eframe::egui::{self, Color32};
+
+pub struct LogDisplay {
+    messages: Vec<String>,
+    failed_deletions: Vec<FailedDeletionFile>,
+}
+
+impl LogDisplay {
+    pub fn new() -> Self {
+        Self {
+            messages : Vec::new(),
+            failed_deletions: Vec::new(),
+        }
+    }
+
+    pub fn log(&mut self, message: &String) {
+        self.messages.push(message.to_string());
+    }
+
+    pub fn log_failed_deletion(&mut self, path: PathBuf, error_message: String) {
+        self.failed_deletions.push(FailedDeletionFile::new(path, error_message));
+    }
+
+    pub fn show(&self, ui: &mut egui::Ui) {
+        for message in &self.messages {
+            ui.label(egui::RichText::new(message).color(Color32::RED));
+        }
+
+        if !self.failed_deletions.is_empty() {
+            ui.separator();
+            ui.label(egui::RichText::new("Failed Deletions:").color(Color32::LIGHT_GRAY));
+
+            for failed in &self.failed_deletions {
+                ui.label(egui::RichText::new(format!("{} - Error: {}", failed.path.display(), failed.error_message))
+                    .color(Color32::RED));
+            }
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.messages.clear();
+        self.failed_deletions.clear();
+    }
+}
 
 
 
